541cdb5a014aa8e12cbd1088014b48977194db1d
patriciax <yively.pa@gmail.com>
2021-04-21 12:52:55 -0400

    Tab content services

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -400,10 +400,49 @@ class Scene {
 
 new Scene(document.querySelector(".c-scene"));
 $(".menu-item").hide();
-$('.menu-1').on("click", function () {
-    $(".menu-item").toggle("show");
-    $(".menu-hidde").addClass('menu-open-button');
-    $("#menu1").addClass('block');
-    
+$(".menu-1").on("click", function () {
+  $(".menu-item").toggle("show");
+  $(".menu-hidde").addClass("menu-open-button");
+  $("#menu1").addClass("block");
 })
+
+
+
+
+  (function () {
+    var indicator = $("#indicator");
+    var counter = indicator.find("span");
+
+    var win = jQuery(window);
+    if (indicator.length) {
+      var moveIndicator = debounce(function () {
+        var viewportHeight = $(window).height();
+        var documentHeight = $(document).height();
+        var hasScrolled = $(window).scrollTop();
+
+        var percent = (hasScrolled / (documentHeight - viewportHeight)) * 100;
+        indicator.css("top", percent + "%");
+        counter.html(Math.floor(percent) + "%");
+      }, 10);
+    }
+
+    win.on("resize scroll", moveIndicator);
+
+    function debounce(func, wait, immediate) {
+      var timeout;
+
+      return function () {
+        var context = this,
+          args = arguments;
+        var later = function () {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
+    }
+  })()
 
